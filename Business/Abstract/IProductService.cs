8a3c04a8a47aff30723d9d3b5396acd383ada542
gmel <gultekin.melike784@gmail.com>
2021-11-11 01:54:29 +0300

    .

--- Business/Abstract/IProductService.cs
+++ Business/Abstract/IProductService.cs
@@ -1,4 +1,6 @@
-﻿using Entities.Concrete;
+﻿using Core.Utilities.Results;
+using Entities.Concrete;
+using Entities.Concrete.DTOs;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -12,5 +14,11 @@ namespace Business.Abstract
         List<Product> GetAllByCategoryId(int id);
 
         List<Product> GetByUnitPrice(decimal min, decimal max);
+
+        List<ProductDetailDto> GetProductDetails();
+
+        Product GetById(int productId);
+
+        IResult Add(Product product);
     }
 }
--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,6 +1,8 @@
 ﻿using Business.Abstract;
+using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
+using Entities.Concrete.DTOs;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -16,6 +18,18 @@ namespace Business.Concrete
             _productDal = productDal;
         }
 
+        public IResult Add(Product product)
+        {
+            //business codes
+            if (product.ProductName.Length<2)
+            {
+                return new ErrorResult("Ürün ismi en az 2 karakter olmalıdır")
+            }
+            _productDal.Add(product);
+            return new Result(true,"Ürün eklendi");
+
+        }
+
         public List<Product> GetAll()
         {
             //İş Kodları
@@ -28,9 +42,19 @@ namespace Business.Concrete
             return _productDal.GetAll(p => p.CategoryId == id);
         }
 
+        public Product GetById(int productId)
+        {
+            return _productDal.Get(p => p.ProductId == productId);
+        }
+
         public List<Product> GetByUnitPrice(decimal min, decimal max)
         {
             return _productDal.GetAll(p => p.UnitPrice >= min && p.UnitPrice <= max);
+        }
+
+        public List<ProductDetailDto> GetProductDetails()
+        {
+            return _productDal.GetProductDetails();
         }
     }
 }
