b7fe5248d94b28c908c8f6e3c18d0c46fd5066aa
gmel <gultekin.melike784@gmail.com>
2021-11-14 22:40:35 +0300

    .

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -30,11 +30,22 @@ namespace ConsoleUI
         {
             ProductManager productManager = new ProductManager(new EfProductDal());
 
-            foreach (var product in productManager.GetProductDetails())
-            {
+            var result = productManager.GetProductDetails();
 
-                Console.WriteLine(product.ProductName + "/" + product.CategoryName);
+            if (result.Success==true)
+            {
+                foreach (var product in result.Data)
+                {
+                    Console.WriteLine(product.ProductName + "/" + product.CategoryName);
+
+                }
+
             }
+            else
+            {
+                Console.WriteLine(result.Message);
+            }
+
         }
     }
 }
