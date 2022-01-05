2272e8b8baa6362e743830b51e07021a91c0721a
gmel <gultekin.melike784@gmail.com>
2022-01-05 15:00:29 +0300

    .

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -4,6 +4,7 @@ using Business.Constants;
 using Business.ValidationRules.FluentValidation;
 using Core.Aspects.Autofac.Validation;
 using Core.CrossCuttingConcerns.Validation;
+using Core.Utilities.Business;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
@@ -19,36 +20,39 @@ namespace Business.Concrete
     public class ProductManager : IProductService
     {
         IProductDal _productDal;
+        ICategoryService _categoryService;
         
 
-        public ProductManager(IProductDal productDal)
+        public ProductManager(IProductDal productDal,ICategoryService categoryService)
         {
             _productDal = productDal;
-           
+            _categoryService = categoryService;
+
+
         }
 
         
         [ValidationAspect(typeof(ProductValidator))]    
         public IResult Add(Product product)
         {
+            IResult result = BusinessRules.Run(CheckIfProductNameExists(product.ProductName),
+              CheckIfProductCountOfCategory(product.CategoryId), CheckIfCategoryLimitExceded());
 
-            //business codes
-
-            if(CheckIfProductCountOfCategory(product.CategoryId).Success)
+            if(result != null)
             {
-                _productDal.Add(product);
-
-                return new SuccessResult(Messages.ProductAdded);
+                return result;
             }
 
-            return new ErrorResult();
+            _productDal.Add(product);
+
+            return new SuccessResult(Messages.ProductAdded);
 
         }
 
         public IDataResult<List<Product>> GetAll()
         {
             //İş Kodları
-            //Yetkisi var mı?
+            //Yetkisi var mı? 
             if (DateTime.Now.Hour == 1)
             {
                 return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
@@ -107,6 +111,17 @@ namespace Business.Concrete
             {
                 return new ErrorResult(Messages.ProductNameAlreadyExists);
             }
+            return new SuccessResult();
+        }
+
+        private IResult CheckIfCategoryLimitExceded()
+        {
+            var result = _categoryService.GetAll();
+            if(result.Data.Count>15)
+            {
+                return new ErrorResult(Messages.CategoryLimitExceded);
+            }
+
             return new SuccessResult();
         }
     }
